fd3354f7fb05142858c71fb2eb043cdad6e592b9
tontim <tontimon+dev@gmail.com>
2024-09-18 11:17:09 +0200

    Added entity models to align with ER diagram. This time to the correct branch...

---
+++ Server/LMS.API/Models/Entities/ActivityType.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace LMS.API.Models.Entities
+{
+    public class ActivityType
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string ActivityTypeName { get; set; }
+
+        public string Type { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Activity> Activities { get; set; }
+    }
+}
---
+++ Server/LMS.API/Models/Entities/Course.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace LMS.API.Models.Entities
+{
+    public class Course
+    {
+        //Primary key Guid
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Course is a required field.")]
+        public string CourseName { get; set; }
+
+        public string Description {  get; set; }
+
+        DateTime StartDate {  get; set; }
+
+        //Navigation property for Artifacts
+        public virtual ICollection<Artifact> Artifacts { get; set; }
+
+    }
+}
