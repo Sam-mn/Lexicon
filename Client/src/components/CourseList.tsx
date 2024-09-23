28341a092f1e65eb73b1a56273bcbaee579c71c3
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-09-23 16:31:21 +0200

    Create ICourse interface

---
+++ Client/src/components/CourseList.tsx
@@ -0,0 +1,5 @@
+import { ReactElement, useState, useEffect } from 'react';
+import { useCourses } from '../hooks/useCourses';
+
+
+import '../css/CourseList.css';
--- Client/src/utils/interfaces.ts
+++ Client/src/utils/interfaces.ts
@@ -1,11 +1,17 @@
 export interface IAuthContext {
-    isLoggedIn: boolean;
-    login: (username: string, password: string) => Promise<void>;
-    logout: () => void;
-  }
-  
-  export interface ITokens {
-    accessToken: string;
-    refreshToken: string;
-  }
-  
+  isLoggedIn: boolean;
+  login: (username: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+export interface ITokens {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface ICourse {
+  id: number;
+  courseName: string;
+  description: string;
+  startDate: string;
+}
